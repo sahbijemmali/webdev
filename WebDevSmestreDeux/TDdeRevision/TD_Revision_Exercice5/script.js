d06188090c090eb49be191d8a72b0a926b1dc046
Sahbi JEMMALI <jemmali.sahbi@gmail.com>
2023-05-21 12:45:37 +0100

    add TD revision exercice 5

---
+++ WebDevSmestreDeux/TDdeRevision/TD_Revision_Exercice5/script.js
@@ -0,0 +1,9 @@
+function changer() {
+  picture = document.getElementById("images");
+  picture.src = "images/pictures.jpeg";
+}
+
+function retourner() {
+  picture = document.getElementById("images");
+  picture.src = "images/picture.jpeg";
+}
